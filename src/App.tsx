2fdd0c784ef27a8091925d7d7d4fffaccc559016
mgfahem <mgfahem@gmail.com>
2025-05-24 21:17:10 +0200

    Update index.html & App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -16,7 +15,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.PROD ? '/honey-web-hive-showcase' : '/'}>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/apiculture" element={<Apiculture />} />
